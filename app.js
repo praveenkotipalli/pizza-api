8e3d6caff5e286ab320998b55865dbca46b1e143
praveen ^_^ <praveenin321@gmail.com>
2024-06-27 17:23:42 +0530

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
-
+const cors = require("cors");
 const pizzaRouter = require("./routes/pizzaRoutes.js");
 
 const app = express();
 
 //MIDDLEWARES
+app.use(cors());
 app.use(express.json());
+
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("Hello from MIDDLEWARE !");
@@ -22,6 +26,10 @@ app.use((req, res, next) => {
 //ROUTE HANDLERS
 
 //ROUTES
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/public/main.html`);
+});
+
 app.use("/api/v1/pizzas", pizzaRouter);
 // app.get("/api/v1/pizzas", getAllPizzas);
 // app.get("/api/v1/pizzas/:id", getPizza);
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app.js");
 
 const port = 8000;
